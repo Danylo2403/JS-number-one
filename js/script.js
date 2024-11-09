152eee3a5361488e3d0591cb6dc964d2f3ba1cd7
Danylo2403 <daniilkon24@gmail.com>
2024-11-09 22:35:54 +0200

    type

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let username;
+console.log(typeof username);
+
+let inputValue = null;
+console.log(typeof inputValue);
+
+const quantity = 17;
+console.log(typeof quantity);
+
+const message = "JavaScript";
+console.log(typeof message);
+
+const isSidebarOpen = false;
+console.log(typeof isSidebarOpen);
